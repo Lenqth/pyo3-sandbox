71913c5d8fefed651e9a89e47c76d2b906132180
Lenqth <nullptr00@gmail.com>
2020-06-28 12:16:06 +0900

    テスト用ファイルの移動

--- a.py
+++
@@ -1,6 +0,0 @@
-from pyon import RowIterator, RowReader
-
-# print(CSVLoader("test.txt").open().read())
-
-for row in RowReader("./test.txt"):
-    print(row)
--- test/test_iter.py
+++ test/test_iter.py
@@ -5,5 +5,8 @@ import unittest
 class IterTest(unittest.TestCase):
 
     def test_file(self):
-        res = list(RowReader("./test.txt"))
+        from pathlib import Path
+        path = str(Path(__file__).parent / "data/test_crlf.txt")
+
+        res = list(RowReader(path))
         self.assertEqual(res, ["12345", "67890"])
